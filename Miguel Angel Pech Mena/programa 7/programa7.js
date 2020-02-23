e192868d9d3a02233a333b425793ce06b394eece
MiguelDeveloperxd <37599874+MiguelDeveloperxd@users.noreply.github.com>
2020-02-22 22:37:42 -0600

    u98o8o

---
+++ Miguel Angel Pech Mena/programa 7/programa7.js
@@ -0,0 +1,8 @@
+/*
++~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*Source: Programa7.js
+*Description: Manejos de tablas.
+*Date: 21/02/2020
+*Author: Miguel Angel Pech Mena
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
