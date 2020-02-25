a580132114f34a6eeee68e9da7858b2d12ac305e
Giovax04 <60788950+Giovax04@users.noreply.github.com>
2020-02-25 11:57:21 -0600

    Actualización

---
+++ CarlosGioRivas/Programa3.js
@@ -0,0 +1,11 @@
+/*
+*source: Programa6.js
+*description: Imprimir varias calificaciones
+*date: --/feb/2020
+*author: Carlos Gio Rivas
+*/
+var calificacion=[];
+for (var i = 0; i < 10; i++) {
+  calificacion[i] = prompt("Escribe tu calificacion");
+}
+alert(calificacion);
