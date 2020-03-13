90c1604d897562cb6df8fbbb1428516b0230a374
Mariolegar <60789219+Mariolegar@users.noreply.github.com>
2020-03-13 11:04:11 -0600

    Creación de Landingpage y login
    Se creó las carpetas y archivos correspondientes a Landingpage y login

---
+++ Mario León García/Programas/U2/Programa16/Index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>	
+	</head>
+	<body>
+
+		<form action="Controller/Verificar.php" method="post">
+		<input type="text" placeholder="Escribe tu nombre" id="inputText" name="inputText">
+		<input type="password" name="inputPassword">
+		<input type="submit" value="Verificar">
+		</form>
+
+	</body>
+</html>
---
+++ Mario León García/Programas/U2/Programa16/View/Login/Login.php
@@ -0,0 +1,12 @@
+<html>
+	<head>	
+	</head>
+	<body>
+		<form action="../../Controller/Verificar.php" method="post">
+			<input type="text" placeholder="Escribe tu nombre" id="inputText" name="inputText">
+			<input type="password" name="inputPassword">
+			<input type="submit" value="Verificar">
+		</form>
+
+	</body>
+</html>
