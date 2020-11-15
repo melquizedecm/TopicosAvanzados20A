0c68facc39bb1a67b25d35888e8fcea477a116c8
CubaCarcedo <cubacarcedo@gmail.com>
2020-11-15 16:33:49 -0600

    commit

---
+++ Gabriel Carcedo Rodríguez/SQLite/BaseDatos.php
@@ -0,0 +1,23 @@
+<?php
+
+	class BaseDatos extends SQLite3
+	{
+		function __construct()
+		{
+			$this->open("Proyecto_Final.db");
+		}
+	}
+
+	$db = new BaseDatos();
+
+	if($db)
+	{
+		echo "<p>La Base de Datos se abrió";
+	}
+	else
+	{
+		echo "<p>La Base de Datos NO se abrió";
+	}
+
+
+?>
